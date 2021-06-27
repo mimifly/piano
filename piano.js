1cae3b2fee31fb5dc8278fbcd277cbb95ecabf0b
mimifly <codemenerd@gmail.com>
2021-06-27 18:47:32 +0100

    path

--- piano.js
+++ piano.js
@@ -53,7 +53,7 @@ window.addEventListener("load", () => {
 async function registerSW() {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", function () {
-      navigator.serviceWorker.register("/sw.js").then(
+      navigator.serviceWorker.register("./sw.js").then(
         function (registration) {
           // Registration was successful
           console.log(
